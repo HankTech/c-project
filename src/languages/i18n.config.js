7ef295373b24ca0f24a63764c56e3d7b5ecf0386
HankTech <HankTech@gmail.com>
2022-02-09 14:47:27 -0400

    configure i18n

---
+++ src/languages/i18n.config.js
@@ -0,0 +1,32 @@
+// import * as Localization from 'expo-localization'
+import * as RNLocalize from 'react-native-localize'
+import i18n from 'i18n-js'
+
+import { es } from './es'
+import { en } from './en'
+
+i18n.defaultLocale = 'en'
+
+// Set the key-value pairs for the different languages you want to support.
+i18n.translations = {
+  es,
+  en
+}
+
+i18n.fallbacks = true
+
+export const initI18n = () => {
+  const language = RNLocalize.findBestAvailableLanguage(['es', 'en'])
+
+  console.log(language)
+
+  if (language?.languageTag) {
+    i18n.locale = language?.languageTag
+  } else {
+    i18n.locale = 'en'
+  }
+}
+
+// Set the locale once at the beginning of your app.
+
+export default i18n
