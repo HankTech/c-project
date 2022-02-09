76f377dd3c72cdc9226e5f2f7855741d4ac74322
HankTech <HankTech@gmail.com>
2022-02-09 14:48:08 -0400

    configure i18n

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navigation from './src/navigation'
+import { initI18n } from './src/languages/i18n.config'
 
 const App = () => {
+  useEffect(() => {
+    initI18n()
+  }, [])
+
   return <Navigation />
 }
 
