f78a5a38fe13b2f416e86e039eb9df37d78ee4c2
HankTech <HankTech@gmail.com>
2022-02-10 13:43:57 -0400

    add sigin screen

--- src/languages/en.js
+++ src/languages/en.js
@@ -3,5 +3,12 @@ export const en = {
   'chating secure with everybody people': 'Chating secure with everybody people',
   continue: 'Continue',
   'take privacy with you': 'Take privacy with you.',
-  'be youself in every message': 'Be youself in every message.'
+  'be youself in every message': 'Be youself in every message.',
+  register: 'Register',
+  'enter a email and password': 'Input a email and password.',
+  email: 'Email',
+  password: 'Password',
+  'this field is required': 'This field is required',
+  'password should be minimon 8 characters long': 'Password should be minimon 8 characters long',
+  'enter a valid email': 'Enter a valid email'
 }
--- src/languages/es.js
+++ src/languages/es.js
@@ -3,5 +3,12 @@ export const es = {
   'chating secure with everybody people': 'Chatea seguro con todo el mundo',
   continue: 'Continuar',
   'take privacy with you': 'Llévate la privacidad contigo.',
-  'be youself in every message': 'Sé tú mismo en cada mensaje.'
+  'be youself in every message': 'Sé tú mismo en cada mensaje.',
+  register: 'Registrarse',
+  'enter a email and password': 'Ingrese un correo y una contraseña.',
+  email: 'Correo',
+  password: 'Contraseña',
+  'this field is required': 'Este campo es obligatorio',
+  'password should be minimon 8 characters long': 'La contraseña debe tener minimo 8 caracteres',
+  'enter a valid email': 'Ingrese un correo valido'
 }
