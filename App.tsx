0d4da2e5d54bb2569af6a431e7f3f3e6c4a65a9d
HankTech <HankTech@gmail.com>
2022-02-11 11:41:10 -0400

    add aws config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react'
 import Navigation from './src/navigation'
 import { initI18n } from './src/languages/i18n.config'
+import Amplify from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
 
 const App = () => {
   useEffect(() => {
