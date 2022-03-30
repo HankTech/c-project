af3fcdbc32e9e3d0b88be2f6eedce30eacd79747
HankTech <HankTech@gmail.com>
2022-03-30 14:57:09 -0400

    auto verify email in lambda function

--- amplify/backend/function/TheProjectCPreSignup/src/custom.js
+++ amplify/backend/function/TheProjectCPreSignup/src/custom.js
@@ -11,6 +11,14 @@ exports.handler = async (event, context) => {
     return
   }
 
+  // Confirm the user
+  event.response.autoConfirmUser = true
+
+  // Set the email as verified if it is in the request
+  if (event.request.userAttributes.hasOwnProperty('email')) {
+    event.response.autoVerifyEmail = true
+  }
+
   const now = new Date()
   const timestamp = now.getTime()
 
