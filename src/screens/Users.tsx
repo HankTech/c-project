315af189d6718bf24701ac99dbc4e6e83847c116
HankTech <HankTech@gmail.com>
2022-03-30 14:42:17 -0400

    add/user screen

--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import { FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, FlatList, StyleSheet } from 'react-native'
+import { DataStore } from '@aws-amplify/datastore'
+import { User } from '../models'
 
 //  components
 import UserItem from '../components/Users/UserItem'
 
-import UsersData from '../../assets/dummy-data/Users'
-
 const Users = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  const fetchUsers = async () => {
+    try {
+      const fetchedUsers = await DataStore.query(User)
+      setUsers(fetchedUsers)
+      console.log(fetchedUsers)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(users)
+
+  useEffect(() => {
+    //  query users
+    fetchUsers()
+  }, [])
+
   return (
-    <FlatList
-      data={UsersData}
-      renderItem={({ item }) => <UserItem user={item} />}
-      showsVerticalScrollIndicator={false}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <UserItem user={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  }
+})
 
 export default Users
